a8e8f384507c4b7c94ea1823eb62aa541c51575a
Campanu Alexandru Catalin <alexandru-catalin.campanu@student.tuiasi.ro>
2022-07-24 15:46:05 +0300

    #login+signup+butoane

--- src/app/core/components/studenttimer/studenttimer.component.ts
+++ src/app/core/components/studenttimer/studenttimer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-studenttimer',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudenttimerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
