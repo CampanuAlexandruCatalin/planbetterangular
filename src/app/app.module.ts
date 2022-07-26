6194e9adf05dbad0efa6b976485435e1b54e00fd
Campanu Alexandru Catalin <alexandru-catalin.campanu@student.tuiasi.ro>
2022-07-26 20:13:51 +0300

    #(admin+student)calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import{MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
@@ -10,8 +17,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule ,
+    MatFormFieldModule,MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/core/components/studentcalendar/studentcalendar.component.ts
+++ src/app/core/components/studentcalendar/studentcalendar.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { CoreModule } from '../../core.module';
 
 @Component({
   selector: 'app-studentcalendar',
   templateUrl: './studentcalendar.component.html',
   styleUrls: ['./studentcalendar.component.scss']
-
 })
 export class StudentcalendarComponent implements OnInit {
   selected: Date | null | undefined;
