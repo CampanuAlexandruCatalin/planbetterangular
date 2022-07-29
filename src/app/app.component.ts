1bcdfb5646f25e97276d7534666c8e663eaa9488
emiliachelariu <100294712+emiliachelariu@users.noreply.github.com>
2022-07-29 13:05:57 +0300

    #

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from './core/services/user.service';
-import { ExamModel } from './shared/models';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit  {
-  exam: ExamModel | undefined;
-  constructor(private userService:UserService){}
+export class AppComponent {
+  constructor(){}
   title = 'planbetter';
-  ngOnInit(): void {
-    this.userService.getExams().subscribe((res:ExamModel)=>{
-    this.exam=res;
-    });
-  }
 }
