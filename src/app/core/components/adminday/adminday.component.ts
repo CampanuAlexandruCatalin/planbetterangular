d986a6730f481f9e9eacf5908f4fc1d9f20eeb27
emiliachelariu <100294712+emiliachelariu@users.noreply.github.com>
2022-07-31 18:21:05 +0300

    #

--- src/app/core/components/adminday/adminday.component.ts
+++ src/app/core/components/adminday/adminday.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { EventRenderedArgs, ScheduleComponent } from '@syncfusion/ej2-angular-schedule';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-adminday',
@@ -7,10 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./adminday.component.scss']
 })
 export class AdmindayComponent implements OnInit {
+  dateTime: Date| null | undefined;
+  @ViewChild('scheduleObj') public scheduleObj: ScheduleComponent | undefined ;
 
-  constructor(public router: Router) { }
+  public selectedDate: Date = new Date();
 
-  ngOnInit(): void {
+
+  public datas: string[] = ['Day'];
+
+  public onEventRendered(args: EventRenderedArgs): void {
+    const categoryColor: string = args.data['CategoryColor'] as string;
+    if (!args.element || !categoryColor) {
+      return;
+    }
+
   }
 
+  constructor(public router: Router) {
+
+  }
+  ngOnInit(): void {
+    this.dateTime=new Date();
+    timer(0,1000).subscribe(()=>{
+      this.dateTime=new Date()
+    })
+
+  }
 }
